9213d993ca1f2c8a24cd92a450f70265d6ef1dc0
maste <maste@DAHYUN>
2024-03-01 07:36:05 -0600

    Agregar archivos de proyecto.

---
+++ Panaderia/Inventario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panaderia
+{
+     public class Inventario : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+
+
+        public void AlAgregar()
+        {
+
+        }
+        public void AlEliminar()
+        {
+
+        }
+    }
+}
---
+++ Panaderia/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panaderia
+{
+     public class Producto
+    {
+        public string RolloCanela { get; set; } = null!;
+        public string PanBlanco { get; set; } = null!;
+        public string Empanada { get; set; } = null!;
+        public string Croissant { get; set; } = null!;
+    }
+}
